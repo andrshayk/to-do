2173a130ee353b0cfa157a563f5e3b13a5c4acf4
andrshayk <andrey.shaykovskiy@gmail.com>
2025-07-15 23:52:52 +0300

    swagger added

--- cmd/db-service/main.go
+++ cmd/db-service/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 
+	_ "github.com/andrdog/nilchan/docs"
 	"github.com/andrdog/nilchan/internal/db/handler"
 	"github.com/andrdog/nilchan/internal/db/storage"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
@@ -16,6 +19,7 @@ func main() {
 
 	r := gin.Default()
 	r.GET("/list", handler.ListTasks)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	log.Println("db-service started on :8081")
 	if err := r.Run(":8081"); err != nil {
--- internal/db/handler/task.go
+++ internal/db/handler/task.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Task представляет задачу чек-листа.
+// @Description Структура задачи
 type Task struct {
 	ID      uint   `gorm:"primaryKey" json:"id"`
 	Title   string `json:"title"`
@@ -18,6 +20,13 @@ func AutoMigrate() {
 	storage.DB.AutoMigrate(&Task{})
 }
 
+// ListTasks возвращает список всех задач.
+// @Summary Получить список задач
+// @Description Возвращает все задачи из базы данных
+// @Tags tasks
+// @Produce json
+// @Success 200 {array} Task
+// @Router /list [get]
 func ListTasks(c *gin.Context) {
 	var tasks []Task
 	storage.DB.Find(&tasks)
